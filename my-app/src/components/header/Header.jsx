5a3f42019259372a667a42bbdf4f33004220a954
StepanTRr <stepano2005@gmail.com>
2024-04-05 14:34:09 +0300

    header add

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss';
+const Header = () => {
+    return (
+        <div>
+            <header className={styles.header}>Hello</header>
+            <p>tttttttttttttttttt</p>
+        </div>
+    );
+}
+export default Header;
