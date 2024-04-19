860246cc0e51f6726c7f19c21d7229cdffd10ed6
StepanTRr <stepano2005@gmail.com>
2024-04-19 21:13:56 +0300

    add like

--- my-app/src/components/header/ProductList/ProductLisr.jsx
+++ my-app/src/components/header/ProductList/ProductLisr.jsx
@@ -4,30 +4,27 @@ import styles from './ProductList.module.scss';
 import { useState, useEffect } from 'react';
 const ProductList = () => {
     const[products, setProducts] = useState([]);
+    const[likedProducts, setLikedProducts] = useState([]);
     const[visibleCount, setVisibleCount] = useState(2);
     const[c, setC] = useState(0);
 
     const showMore = () => {
-        //alert('click');
         setVisibleCount(count => count + 2);
     }
-    const onclickbtn =() => {
-        setC(c => c + 1);
-        alert(c);
-    }
-    // like: useState
     useEffect(() => {
         setProducts(data);
     }, [])
 
     const LikeProduct = (id) => {
-        const updateProducts = products.filter((item) => {
+        const updateProducts = products.map((item) => {
             if (item.id == id) {
-                item.Like = !item.Like;
-                return null;
+                return {...item, Like: !item.Like};
             }
-        })
+            return item;
+        });
         setProducts(updateProducts);
+        const liked = updateProducts.filter((item => item.Like));
+        setLikedProducts(liked);
     }
 
     const deleteProduct = (id) => {
@@ -41,7 +38,7 @@ const ProductList = () => {
         <div className={styles.div_list}>
             {products.slice(0, visibleCount).map((item, index) => (
                 <div key = {index}>
-                    <Product product = {item} deleteProduct = {deleteProduct}/>
+                    <Product product = {item} deleteProduct = {deleteProduct} likeProduct = {LikeProduct}/>
                     
                 </div>
             ))}
@@ -50,7 +47,15 @@ const ProductList = () => {
         {visibleCount < products.length && (
             <button onClick={showMore} className={styles.button_add}>Еще</button>
         )}
-        <button onClick={onclickbtn}>ооо</button>
+        </div>
+        <h2 className={styles.h2_name}>Понравившееся</h2>
+        <div className={styles.div_list_like}>
+            {likedProducts.map((item, index) => (
+                <div key = {index}>
+                    <Product product = {item} deleteProduct = {deleteProduct} likeProduct = {LikeProduct}/>
+                    
+                </div>
+            ))}
         </div>
         </>
     );
