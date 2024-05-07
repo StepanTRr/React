a75c07a7cf4cd62605ae68befb8e2ac5700d67bf
StepanTRr <stepano2005@gmail.com>
2024-05-07 17:08:52 +0300

    MUI add

--- my-app/src/components/header/ProductList/API.jsx
+++ my-app/src/components/header/ProductList/API.jsx
@@ -3,10 +3,38 @@ import styles from './ProductList.module.scss';
 import styles_api from './api.module.scss';
 import Product_API from './Product_api.jsx'
 import Loading from './../../../Pages/Loading.jsx';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
 const API = () => {
     const[products, setProducts] = useState([]);
     const[visibleApi, setVisibleApi] = useState(6);
-
+    const[textModal, setTextModal] = useState('');
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        pt: 2,
+        px: 4,
+        pb: 3,
+      };
+      const [open, setOpen] = useState(false);
+      const handleOpen = () => {
+        setOpen(true);
+      };
+      const handleClose = () => {
+        setOpen(false);
+      };      
     const ShowMore = () => {setVisibleApi(visibleApi => visibleApi + 9)}
 
     const [isLoading, setIsLoading] = useState(true);
@@ -27,20 +55,41 @@ const API = () => {
         <div className={styles_api.main}>
         <h1>API</h1><div className={styles_api.div_list}>
                 {products.slice(0, visibleApi).map((item, index) => (
-                    <div key={index}>
-                        <Product_API product={item}></Product_API>
-                    </div>
+                    <Card key={index} sx={{height: 650}}>
+                        <CardContent>
+
+                            <Product_API product={item}></Product_API>
+                        </CardContent>
+                        <CardActions >
+                            <Button variant="contained" onClick={() => {setTextModal(item.description); handleOpen();}}>View</Button>
+                            
+                        </CardActions>
+                    </Card>
                 ))}
             </div><div className={styles_api.div_btn}>
                     {visibleApi < products.length && (
-                        <button onClick={ShowMore} className={styles_api.button_add}>Еще</button>
+                        <Button onClick={ShowMore} variant="contained" className={styles_api.button_add}>Еще</Button>
                     )}
                 </div>
         </div>
         )
         
     }
-        
+    <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      Описание товара
+    </Typography>
+    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      {textModal}
+    </Typography>
+  </Box>
+</Modal>
     </div>
 }
 export default API;
--- my-app/src/components/header/ProductList/Product_api.jsx
+++ my-app/src/components/header/ProductList/Product_api.jsx
@@ -1,13 +1,13 @@
 
 import styles from '../ProductList/api.module.scss';
-
+import Typography from '@mui/material/Typography';
 const Product_API = (props) => {
     const{product} = props;
     return (
         <div className={styles.div_card}>
             <div className={styles.div_up_product}>
             <img src={product.images[0]} alt="ICON" className={styles.image}/>
-                <p className={styles.p_def__main}>{product.title}</p>
+                <Typography variant='h5'>{product.title}</Typography>
                 
             </div>
             <p className={styles.p_def}><del>{product.price}$</del></p>
